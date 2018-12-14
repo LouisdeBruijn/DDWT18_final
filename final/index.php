4a8eac9ed3f482ff98264f52a8eb09d8b22f9797
MalouBron <m.bron.3@student.rug.nl>
2018-12-14 11:55:20 +0100

    new route for "edit user account get"

--- final/index.php
+++ final/index.php
@@ -208,6 +208,7 @@ elseif (new_route('/DDWT18/final/add/', 'get')) {
     include use_template('new-step1');
 }
 
+
 /* Add serie POST */
 elseif (new_route('/DDWT18/final/add/', 'post')) {
 
@@ -252,6 +253,29 @@ elseif (new_route('/DDWT18/final/add/', 'post')) {
         redirect(sprintf('/DDWT18/final/add/?error_msg=%s', json_encode($error_msg)));
     }
 
+}
+
+/* Edit user account get */
+elseif (new_route('/DDWT18/final/edit/', 'get')) {
+    /* Get user account information from db */
+    check_login();
+    $navigation = get_navigation($navigation_tpl, 5);
+    $name = get_username($db, get_user_id());
+    $page_title = 'edit account';
+    $page_subtitle = 'edit here your personal account';
+    $page_content = '';
+    $user_id = $_GET['user_id'];
+    $user_info = get_account_info($db, $user_id);
+    $user_firstname = $user_info['firstname'];
+    $user_lastname = $user_info['lastname'];
+    $user_birthdate = $user_info['birthdate']; #je kan niet veranderen van tenant of owner dus die hebben we er niet in
+    $user_biography = $user_info['biography'];
+    $user_occupation = $user_info['occupation'];
+    $user_language = $user_info['language'];
+    $user_email = $user_info['email'];
+    $user_phone = $user_info['phone'];
+
+    include use_template('account');
 }
 
 else {
